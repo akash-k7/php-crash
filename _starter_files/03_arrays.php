5002ee7ca642d9d8b47593f5406473387fd6f040
AKASH K <92331024+akash-k7@users.noreply.github.com>
2022-11-29 16:04:30 +0530

    Update 03_arrays.php

--- _starter_files/03_arrays.php
+++ _starter_files/03_arrays.php
@@ -1,6 +1,48 @@
 <?php
 /* ----------- Arrrays ----------- */
-
+$nums =[1,12,22];
+$names=['me','you','them'];
+print_r($names); 
+var_dump($nums);
 /*
   Arrays hold multiple values. Each value in an array is called an "element"
 */
+
+//assigning custom index for array values and printing specific index value
+$hex=[
+    'red' => '#f00',
+    'black' => '#000',
+    'white'=> '#fff'
+];
+print_r($hex);
+echo $hex['black'];
+
+$person=[
+ 'name' => 'Akash',
+ 'codepen' => 'www.codepen.com/akash-k7',
+ 'github' => 'www.github.com/akash-k7' 
+];
+
+echo $person['codepen'];
+
+$users =[
+    [
+        'name' => 'Akash',
+        'codepen' => 'www.codepen.com/akash-k7',
+        'github' => 'www.github.com/akash-k7' 
+    ],
+    [
+        'name' => 'user1',
+        'codepen' => 'www.codepen.com/user1',
+        'github' => 'www.github.com/user1' 
+    ],
+    [
+        'name' => 'user2',
+        'codepen' => 'www.codepen.com/user2',
+        'github' => 'www.github.com/user2' 
+    ]
+];
+
+echo $users[1]['github'];
+
+
